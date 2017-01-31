36a9cbd9094c9b5799f5bdaa610b0f4ce9d10cd7
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-01-31 18:21:38 -0500

    some changes, this will eventually be individually updated to the original file.

--- js/init.js
+++ js/init.js
@@ -1,3 +1,6 @@
+var keywordName=$("#searchinput").val();
+
+
 (function($){
   $(function(){
 
@@ -8,6 +11,13 @@
 })(jQuery); // end of jQuery name space
 
  $(document).ready(function(){
-      $('.carousel').carousel();
-      $('.carousel.carousel-slider').carousel({fullWidth: true});
+      $('.carousel.carousel-slider').carousel({
+        shift:0,
+        padding:200,
+        indicators:true,
+        fullWidth: true,
+      });
     });
+
+
+$("#searchedKeywordTitle").html(keywordName)
