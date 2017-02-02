35c30e20b09775cfda423874d71559c980607a6d
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-02-01 21:42:37 -0500

    added modal

--- js/init.js
+++ js/init.js
@@ -8,12 +8,15 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
- $(document).ready(function(){
-      $('.carousel').carousel({
-        shift:0,
-        indicators:true,
-        
-        fullWidth: true,
-      });
-       $('.modal').modal();
+$(document).ready(function(){
+    $('.carousel').carousel({
+      shift:0,
+      indicators:true,
+      fullWidth: true,
     });
+
+    $('.modal').modal({
+        opacity: .5,
+        endingBottom: '80%',
+    });
+});
